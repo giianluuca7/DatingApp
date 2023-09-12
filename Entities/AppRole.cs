fd478f3cab3687f9bc8c2005b6104181d9b10da8
Gian Luca <gorgerinogianluca@gmail.com>
2023-09-12 09:16:47 -0300

    Commit por las dudas

---
+++ Entities/AppRole.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace Api.Entities
+{
+    public class AppRole : IdentityRole<int>
+    {
+        public ICollection<AppUserRole> UserRoles { get; set; }
+    }
+}
